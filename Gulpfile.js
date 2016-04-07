fa970c4923690e8c5cdcfb0e37ad52fd10fdced4
0xNacho <nachogarcia91@gmail.com>
2016-04-07 13:48:34 +0200

    Replace double quote with single  quote on Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,24 @@
-const gulp = require("gulp");
-const gutil = require("gulp-util");
-const uglify = require("gulp-uglify");
-const concat = require("gulp-concat");
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 
-gulp.task("concat", function() {
+gulp.task('concat', function() {
   return gulp.src([
-  	"./src/utils/globals.js",
-  	"./src/utils/svg.js",
-  	"./src/charts/classes.js"
+  	'./src/utils/globals.js',
+  	'./src/utils/svg.js',
+  	'./src/charts/classes.js'
   	])
-    .pipe(concat("proteus-charts.js"))
-    .pipe(gulp.dest("./dist/"));
+    .pipe(concat('proteus-charts.js'))
+    .pipe(gulp.dest('./dist/'));
 });	
 
-gulp.task("minify", function() {
-  return gulp.src("./dist/es5/proteus-charts.js")
-    .pipe(uglify().on("error", gutil.log))
-    .pipe(gulp.dest("./min"));
+gulp.task('minify', function() {
+  return gulp.src('./dist/es5/proteus-charts.js')
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(gulp.dest('./min'));
   
 });
 
-gulp.task("default", ["concat", "minify"]);
+gulp.task('default', ['concat', 'minify']);
