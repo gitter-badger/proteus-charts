597f572d830728cd4b28615c689a2c289d1065fe
0xNacho <nachogarcia91@gmail.com>
2016-04-07 15:26:12 +0200

    Restore Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,32 @@ const gulp = require('gulp');
 const gutil = require('gulp-util');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const server = require('karma').Server;
 
 
 gulp.task('concat', function() {
   return gulp.src([
-  	'./src/utils/globals.js',
-  	'./src/utils/svg.js',
-  	'./src/charts/classes.js'
-  	])
-    .pipe(concat('proteus-charts.js'))
-    .pipe(gulp.dest('./dist/'));
-});	
+    './src/utils/globals.js',
+    './src/utils/svg.js',
+    './src/charts/classes.js',
+    './src/charts/barchart.js'
+    './src/charts/streamgraph.js'
+  ])
+  .pipe(concat('proteus-charts.js'))
+  .pipe(gulp.dest('./dist/'));
+}); 
 
 gulp.task('minify', function() {
   return gulp.src('./dist/es5/proteus-charts.js')
     .pipe(uglify().on('error', gutil.log))
     .pipe(gulp.dest('./min'));
-  
 });
 
-gulp.task('default', ['concat', 'minify']);
+gulp.task('test', function (done) {
+  new server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('default', ['test', 'concat', 'minify']);
