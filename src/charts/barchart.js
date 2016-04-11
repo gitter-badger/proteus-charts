072a1f7187ee6d65d6f75cd9cbf425b873974bb5
0xNacho <nachogarcia91@gmail.com>
2016-04-11 13:09:15 +0200

    use of single quotes instead of double ones

--- src/charts/barchart.js
+++ src/charts/barchart.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Barchart implementation. This charts belongs to 'Basic' family.
--- src/charts/classes.js
+++ src/charts/classes.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /* globals Svg, _default */
 
 /**
