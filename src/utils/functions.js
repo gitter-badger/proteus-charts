f86f78d1d8612349025bbdb21cbea3aa16e4a08f
0xNacho <nachogarcia91@gmail.com>
2016-04-12 11:46:42 +0200

    add new util function: getArrayDifferentKeys

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -4,5 +4,13 @@ var utils = {
 	},
 	isObject (d) {
 		return d && d.constructor === Object && d instanceof Object;
-	}
+	},
+  getArrayDifferentKeys(array, field){
+    var keys = [];
+    for(let i = 0; i < array.length; i++){
+      var element = field ? array[i][field] : array[i];
+      keys.push(element);
+    }
+    return d3.set(keys).size();
+  }
 };
